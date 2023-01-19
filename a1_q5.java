42bc0e1dcbda78622bf6e83f03f93c149deef63f
Nandini-Pandey <120239212+Nandini-Pandey@users.noreply.github.com>
2023-01-20 00:29:38 +0530

    submitting a1_q1

---
+++ a1_q5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+  
+  public class a1_q5{
+  
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n= sc.nextInt();
+    int d=0;
+    while(n!=0){
+         n= n/10;
+         d++;
+
+    System.out.println(d);
+
+
+
+    }
+    
+   }
+   }
+  
